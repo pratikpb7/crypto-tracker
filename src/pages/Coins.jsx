f8feef99e7bd134a28e444614b78629a2a061758
Pratik Bagalkoti <pratikbagalkoti23@gmail.com>
2026-06-15 19:03:28 +0530

    Added coin list with API integration

--- src/pages/Coins.jsx
+++ src/pages/Coins.jsx
@@ -1,4 +1,34 @@
+import { useState,useEffect } from "react";
+import { getCoins } from "../services/coinService";
 export default function Coins() {
-  return <h1>Coin Page</h1>;
-}
+  const [coins, setCoins] = useState([]);
+  const fetchCoins = async () => {
+    try {
+      const data = await getCoins();
+      setCoins(data);
+    } catch (error) {
+      console.error("Failed to fetch coins", error);
+    }
+  };
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+  return (
+   <div className="p-4">
+  {coins.map((coin) => (
+    <div
+      key={coin.id}
+      className="border p-4 rounded-lg mb-4 shadow"
+    >
+      <h2 className="text-xl font-bold">
+        {coin.name}
+      </h2>
 
+      <p>{coin.symbol.toUpperCase()}</p>
+
+      <p>${coin.current_price}</p>
+    </div>
+  ))}
+</div>
+  );
+}
