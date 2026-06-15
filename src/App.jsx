d5d5a11190d1e97b5fa48427dab712c90a367019
Pratik Bagalkoti <pratikbagalkoti23@gmail.com>
2026-06-15 13:12:57 +0530

    Added navbar navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <AppRoutes/>
      
     </div>
--- src/pages/Coins.jsx
+++ src/pages/Coins.jsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 export default function Coins() {
-  return <h1>Coins Page</h1>;
+  return <h1>Coin Page</h1>;
 }
 
-=======
-export default function Coins(){
-    return(
-        <div>Coins Page</div>
-    )
-}
->>>>>>> 51bba01626970c304e9ccd8819bc9f7f810af98e
