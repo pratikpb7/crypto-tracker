f70311c65d336a511b66d70d906174b7e4393a8d
Pratik Bagalkoti <pratikbagalkoti23@gmail.com>
2026-06-15 19:31:23 +0530

    Added coin listing with loading and error states

--- src/pages/Coins.jsx
+++ src/pages/Coins.jsx
@@ -1,34 +1,41 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getCoins } from "../services/coinService";
 export default function Coins() {
   const [coins, setCoins] = useState([]);
+  const [loading,setLoading]=useState(true)
+  const [error, setError] = useState("");
   const fetchCoins = async () => {
     try {
       const data = await getCoins();
       setCoins(data);
     } catch (error) {
-      console.error("Failed to fetch coins", error);
+      setError("Failed to fetch coins");
     }
+    finally {
+    setLoading(false);
+  }
   };
   useEffect(() => {
     fetchCoins();
   }, []);
+  if (loading) {
+  return <h1>Loading...</h1>;
+}
+
+if (error) {
+  return <h1>{error}</h1>;
+}
   return (
-   <div className="p-4">
-  {coins.map((coin) => (
-    <div
-      key={coin.id}
-      className="border p-4 rounded-lg mb-4 shadow"
-    >
-      <h2 className="text-xl font-bold">
-        {coin.name}
-      </h2>
+    <div className="p-4">
+      {coins.map((coin) => (
+        <div key={coin.id} className="border p-4 rounded-lg mb-4 shadow">
+          <h2 className="text-xl font-bold">{coin.name}</h2>
 
-      <p>{coin.symbol.toUpperCase()}</p>
+          <p>{coin.symbol.toUpperCase()}</p>
 
-      <p>${coin.current_price}</p>
+          <p>${coin.current_price}</p>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
   );
 }
