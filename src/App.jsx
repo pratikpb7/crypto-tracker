ba4819ca45746c9318b426989623e09c852e6d7a
Pratik Bagalkoti <pratikbagalkoti23@gmail.com>
2026-06-15 13:03:03 +0530

    Added navbar navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-=======
-import Navbar from "./components/Navbar";
->>>>>>> 51bba01626970c304e9ccd8819bc9f7f810af98e
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <AppRoutes/>
      
-=======
-      <Navbar/>
-      <AppRoutes/>
-      
->>>>>>> 51bba01626970c304e9ccd8819bc9f7f810af98e
     </div>
   );
 }
